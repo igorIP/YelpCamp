5fce9c09676537047a86c4a16148c164c9e7be71
Igor Pavlov <igor.pavlov.email@gmail.com>
2019-02-24 04:22:39 +0100

    Inplemented Passport.js(local strategy), sign up, login and logout routes

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 //@ts-check
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var mongoose = require('mongoose');
-// var mongo   = require("mongodb");
-var assert = require("assert");
-var Campground = require("./models/campground");
-var Comment = require("./models/comment");
-seedDB = require("./seeds");
-
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require('mongoose'),
+    //assert = require("assert"),
+    Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
+    passport = require('passport'),
+    bodyParser = require('body-parser'),
+    LocalStrategy = require('passport-local'),
+    User = require("./models/user"),
+    passportLocalMongoose = require('passport-local-mongoose'),
+    seedDB = require("./seeds");
 
 
 mongoose.connect("mongodb://localhost/yelp_camp");
@@ -16,6 +19,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
+
+//Pasport Configuration
+
+app.use(require("express-session")({
+    secret: "This is a big secret",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function (req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.get("/", function (req, res) {
     res.render("landing");
@@ -29,7 +50,7 @@ app.get("/campgrounds", function (req, res) {
         if (err) {
             console.log(err);
         } else {
-            res.render("campgrounds/index", { campgrounds: allCampgrounds });
+            res.render("campgrounds/index", { campgrounds: allCampgrounds, currentUser: req.user });
         }
     });
 });
@@ -72,7 +93,7 @@ app.get("/campgrounds/:id", function (req, res) {
 //COMMENTS Routes
 //=========================
 
-app.get("/campgrounds/:id/comments/new", function (req, res) {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function (req, res) {
     Campground.findById(req.params.id, function (err, campground) {
         if (err) {
             console.log(err);
@@ -82,7 +103,7 @@ app.get("/campgrounds/:id/comments/new", function (req, res) {
     })
 })
 
-app.post("/campgrounds/:id/comments", function (req, res) {
+app.post("/campgrounds/:id/comments", isLoggedIn, function (req, res) {
     Campground.findById(req.params.id, function (err, campground) {
         if (err) {
             res.redirect("/campgrounds");
@@ -101,6 +122,56 @@ app.post("/campgrounds/:id/comments", function (req, res) {
 });
 
 //=========================
+
+//Sign Up, Login Logout Routes
+
+//sign up
+app.get("/register", function (req,res) {
+    res.render("register")
+})
+
+app.post("/register", function (req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register"); 
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        });
+    });
+});
+
+//login
+
+app.get("/login", function (req,res) {
+    res.render("login");
+});
+
+
+app.post("/login",  
+    passport.authenticate('local', 
+    { 
+        successRedirect: '/', 
+        failureRedirect: '/login' 
+    }), function(req, res) {} 
+);
+
+//logout
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/campgrounds");
+})
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
+//==========
 app.listen(3000, function () {
     console.log("App Server STARTED");
 });
