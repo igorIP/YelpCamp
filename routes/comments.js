f6f5a9fbc388c17b645311a2e10c06c5bc445c71
Igor Pavlov <igor.pavlov.email@gmail.com>
2019-02-26 20:00:00 +0100

    added comment delete

--- routes/comments.js
+++ routes/comments.js
@@ -59,6 +59,18 @@ router.put("/:comment_id", function (req, res) {
     })
 })
 
+//Comments Destroy
+router.delete("/:comment_id", function (req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function (err, deletedComment) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    })
+})
+
+
 //middleware
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
