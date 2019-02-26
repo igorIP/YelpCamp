beecb472c224eff4a49ca565a4de161a6f0ee1b0
Igor Pavlov <igor.pavlov.email@gmail.com>
2019-02-26 20:43:40 +0100

    add comment authorization

--- routes/comments.js
+++ routes/comments.js
@@ -38,7 +38,7 @@ router.post("/", isLoggedIn, function (req, res) {
 });
 
 //Comments Edit
-router.get("/:comment_id/edit", function (req, res) {
+router.get("/:comment_id/edit", checkCommentOwnership, function (req, res) {
     Comment.findById(req.params.comment_id, function (err, foundComment) {
         if (err) {
             res.redirect("back")
@@ -60,7 +60,7 @@ router.put("/:comment_id", function (req, res) {
 })
 
 //Comments Destroy
-router.delete("/:comment_id", function (req, res) {
+router.delete("/:comment_id", checkCommentOwnership, function (req, res) {
     Comment.findByIdAndRemove(req.params.comment_id, function (err, deletedComment) {
         if (err) {
             res.redirect("back");
@@ -77,6 +77,25 @@ function isLoggedIn(req, res, next) {
         return next();
     }
     res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function (err, comment) {
+            if (err) {
+                res.redirect("back");
+            }
+            else {
+                if (comment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
 }
 
 module.exports = router;
